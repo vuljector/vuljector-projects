87e41c5082daaff2c9b93aaf843aac4abb6951b8
AmL-Dev <aml1234@laposte.net>
2026-03-24 15:15:54 -0400

    fix test success count

--- libressl/unit_tests/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /src/libressl
-unset SANITIZER_FLAGS LIB_FUZZING_ENGINE
-export CFLAGS="" CXXFLAGS="" LDFLAGS=""
-mkdir -p /tmp/build && cd /tmp/build
-cmake /src/libressl -DCMAKE_BUILD_TYPE=Debug -DLIBRESSL_TESTS=ON -DBUILD_TESTING=ON 2>&1 | tail -1
-cmake --build . -j$(nproc) 2>&1 | tail -1
-ctest --output-on-failure 2>&1 | python3 /src/unit_tests/parse_results.py --framework ctest
